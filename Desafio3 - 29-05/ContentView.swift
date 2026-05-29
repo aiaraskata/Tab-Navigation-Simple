41deb92c4f91d0aa584478d99ad6f33f9bd4c98b
aiaraskata <iara23211232@gmail.com>
2026-05-29 13:27:30 -0300

    Initial Commit

---
+++ Desafio3 - 29-05/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Desafio3 - 29-05
+//
+//  Created by Turma01-15 on 29/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
